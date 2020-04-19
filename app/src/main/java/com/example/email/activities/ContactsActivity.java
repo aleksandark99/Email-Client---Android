f2dda58604ca5b30c38a63699ade486c4283aed3
MiljanGitHub <miljan.puletic@gmail.com>
2020-04-19 14:05:37 +0200

    Added ContactNavItem model class

--- app/src/main/java/com/example/email/activities/ContactsActivity.java
+++ app/src/main/java/com/example/email/activities/ContactsActivity.java
@@ -1,26 +1,82 @@
 package com.example.email.activities;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.example.email.R;
 import com.example.email.adapters.ContactsAdapter;
 import com.example.email.repository.Repository;
 
+import java.util.ArrayList;
+
 public class ContactsActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
+
+    private DrawerLayout drawerLayout;
+    private RelativeLayout drawerPane;
+    private ListView drawerList;
+    private ActionBarDrawerToggle drawerToggle;
+    private ArrayList<ProfileNavItem> mNavItems = new ArrayList<ProfileNavItem>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        prepareDrawerItems();
+
+
+        drawerList = (ListView) findViewById(R.id.drawerList);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawerPane = (RelativeLayout) findViewById(R.id.drawerPane);
+
+        drawerList.setAdapter(new ProfileAdapter(this, mNavItems));
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            //actionBar.setIcon(R.drawable.ic_launcher);
+            //actionBar.setHomeAsUpIndicator(R.drawable.ic_drawer);
+            actionBar.setHomeButtonEnabled(true);
+        }
+
+        drawerList.setOnItemClickListener(new DrawerItemClickListener(this));
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open_drawer, R.string.close_drawer) {
+            //Called when a drawer has settled in a completely closed state
+            @Override
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+            //Called when a drawer has settled in a completely open state.
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        drawerLayout.addDrawerListener(drawerToggle);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // getSupportActionBar().setHomeButtonEnabled(true);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.contacts_recycler);
         ContactsAdapter adapter = new ContactsAdapter(Repository.get(this).getContacts(),this);
