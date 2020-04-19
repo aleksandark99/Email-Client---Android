2ade6a4b60bb4bd7797b125c0d4d6c3ac0b5cefa
MiljanGitHub <miljan.puletic@gmail.com>
2020-04-19 12:42:49 +0200

    Added Helper class

---
+++ app/src/main/java/com/example/email/utility/Helper.java
@@ -0,0 +1,12 @@
+package com.example.email.utility;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.email.R;
+
+public class Helper {
+
+    
+}
