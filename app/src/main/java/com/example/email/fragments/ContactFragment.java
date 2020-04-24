cbc7f00431913baa742ab16652ebaeba4a1edd15
MiljanGitHub <miljan.puletic@gmail.com>
2020-04-24 15:34:04 +0200

    added some changes

--- app/src/main/java/com/example/email/fragments/ContactFragment.java
+++ app/src/main/java/com/example/email/fragments/ContactFragment.java
@@ -143,14 +143,6 @@ public class ContactFragment extends Fragment {
         }
     }
 
-    /*    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-        }
-    }*/
 
     public File getPhotoFile() {
         File externalFilesDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
